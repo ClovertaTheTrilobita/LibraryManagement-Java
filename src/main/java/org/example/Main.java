01e796fc674810164f9148ffc48164fa8608e22c
LXY-fear <316002990@qq.com>
2025-03-16 20:18:40 +0800

    加了一行12345

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!11111");
-        System.out.println("这是第二行。");
+        System.out.println("这是第二行。12345");
     }
 }
