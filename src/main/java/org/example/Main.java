df608a2571a746402f305377b503abe683590ed0
Mioice <yz17347413996@163.com>
2025-03-16 20:00:19 +0800

    加了一行输出

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!11111");
+        System.out.println("这是第二行。");
     }
 }
