2f81a6620d35f273a6ff4dcf0e507bbe0107d041
ClovertaTheTrilobita <cloverta@petalmail.com>
2025-04-07 17:48:14 +0800

    增加借书还书功能

---
+++ src/main/java/org/librarymanagment/database/BorrowInfo.java
@@ -0,0 +1,51 @@
+package org.librarymanagment.database;
+
+import java.sql.Timestamp;
+
+public class BorrowInfo {
+    private int borrowId;
+    private int userId;
+    private int bookId;
+    private Timestamp borrowTime;
+    private Timestamp returnTime;
+
+    public int getBorrowId() {
+        return borrowId;
+    }
+
+    public void setBorrowId(int borrowId) {
+        this.borrowId = borrowId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public Timestamp getBorrowTime() {
+        return borrowTime;
+    }
+
+    public void setBorrowTime(Timestamp borrowTime) {
+        this.borrowTime = borrowTime;
+    }
+
+    public Timestamp getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Timestamp returnTime) {
+        this.returnTime = returnTime;
+    }
+}
